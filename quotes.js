d2b1f9e628ba027533a868d94033c84df2d47e8c
Kuat <Kuatso88@gmail.com>
2025-01-28 20:44:26 +0600

    add quotes card, add function toggle add-del cards

--- quotes.js
+++ quotes.js
@@ -2,30 +2,37 @@ const posts = [
   {
     quote: "Лучше быть богатым чем бедным",
     author: "Keke",
+    isFavorite: false,
   },
   {
     quote: "Лучше иметь 100 биткойнов чем сто баксов",
     author: "Keke",
+    isFavorite: false,
   },
   {
     quote: "Надо было закупиться битками в 2010",
     author: "Keke",
+    isFavorite: false,
   },
   {
     quote: "Хранить крипту в холодном кошельке",
     author: "Keke",
+    isFavorite: false,
   },
   {
     quote: "Всё начинается с первого биткойна",
     author: "Keke",
+    isFavorite: false,
   },
   {
     quote: "Держи свои ключи при себе",
     author: "Keke",
+    isFavorite: false,
   },
   {
     quote: "Не торопись продавать — торопись покупать",
     author: "Keke",
+    isFavorite: false,
   },
   {
     quote: "Мемы — это тоже валюта",
@@ -38,14 +45,17 @@ const posts = [
   {
     quote: "Криптобезопасность — это твоя ответственность",
     author: "Keke",
+    isFavorite: false,
   },
   {
     quote: "HODL спасёт мир",
     author: "Keke",
+    isFavorite: false,
   },
   {
     quote: "Крипта любит терпеливых",
     author: "Keke",
+    isFavorite: false,
   },
 ];
 
